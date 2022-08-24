0c700a542b84b7212eed9c7ff7c9cbf3be833330
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2022-08-24 11:19:49 -0400

    first commit

---
+++ src/main/java/com/example/emailevents/EmailEventsApplication.java
@@ -0,0 +1,13 @@
+package com.example.emailevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailEventsApplication.class, args);
+    }
+
+}
