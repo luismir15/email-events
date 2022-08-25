0f5911a7edb37d2535650ec89dffe97d6df0db87
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2022-08-25 16:26:18 -0400

    implemented GET method

--- src/main/java/com/example/emailevents/controller/EventController.java
+++ src/main/java/com/example/emailevents/controller/EventController.java
@@ -1,12 +1,13 @@
 package com.example.emailevents.controller;
 
 import com.example.emailevents.model.Event;
-import com.example.emailevents.repo.EventRepo;
 import com.example.emailevents.service.EventService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -14,9 +15,6 @@ import java.util.List;
 public class EventController {
 
     private final EventService eventService;
-
-    @Autowired
-    private EventRepo eventRepo;
 
     @Autowired
     public EventController(EventService eventService) {
@@ -32,9 +30,31 @@ public class EventController {
     }
 
     @GetMapping(value = "/events")
+    public List<Event> getEvents(
+            @RequestParam(required = false) String action,
+            @RequestParam(required = false) String recipient,
+            @RequestParam(required = false) String timestamp
+    ) {
+
+        Event event = new Event();
+
+        if (!StringUtils.isBlank(action) || !StringUtils.isBlank(recipient)) {
+            if (!StringUtils.isBlank(timestamp)) {
+                String updatedTimestampString = timestamp.replace('T', ' ');
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SS");
+                event.setTimestamp(LocalDateTime.parse(updatedTimestampString, formatter));
+            }
+            event.setAction(action);
+            event.setRecipient(recipient);
+        }
+
+        return eventService.getEvents(event);
+    }
+
+    @GetMapping(value = "/events/all")
     public List<Event> getAllEvents() {
 
-        return eventRepo.findAll();
+        return eventService.getAllEvents();
     }
 
 }
