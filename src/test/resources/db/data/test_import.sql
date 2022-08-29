db941288e9581d50003aec0038ac8797876d509d
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2022-08-29 11:12:27 -0400

    setting up test cases and testing repo

---
+++ src/test/resources/db/data/test_import.sql
@@ -0,0 +1,14 @@
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174000', 'click', 'Subscribe Now', 'eric@piloto151.com', '2021-02-11T13:57:35.780Z');
+
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174001', 'open', 'Important Message', 'luis@pr.com', '2021-03-11T13:57:35.780Z');
+
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174002', 'click', 'Subscribe Now', 'carlos@titos.com', '2021-04-11T13:57:35.780Z');
+
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174003', 'click', 'Subscribe Now', 'javier@aldo.com', '2021-05-11T13:57:35.780Z');
+
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174004', 'open', 'Subscribe Now', 'paola@disney.com', '2021-07-11T13:57:35.780Z');
