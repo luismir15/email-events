6dc8eb86273d85ca3691feafced9b12c8784a1da
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2022-08-29 20:24:20 -0400

    adding new values for test cases

--- src/test/resources/db/data/test_import.sql
+++ src/test/resources/db/data/test_import.sql
@@ -1,5 +1,11 @@
 INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
-VALUES ( '123e4567-e89b-12d3-a456-426614174000', 'click', 'Subscribe Now', 'eric@piloto151.com', '2021-02-11T13:57:35.780Z');
+VALUES ( '123e4567-e89b-12d3-a456-426614174000', 'click', 'Subscribe Now', 'eric@piloto151.com', '2020-02-11T13:57:35.780Z');
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174010', 'click', 'Subscribe Now', 'eric@piloto151.com', '2020-03-11T13:57:35.780Z');
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174020', 'click', 'Subscribe Now', 'eric@piloto151.com', '2020-04-11T13:57:35.780Z');
+INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
+VALUES ( '123e4567-e89b-12d3-a456-426614174030', 'click', 'Subscribe Now', 'eric@piloto151.com', '2020-05-11T13:57:35.780Z');
 
 INSERT INTO event (event_id, event_action, event_subject, event_recipient, event_timestamp)
 VALUES ( '123e4567-e89b-12d3-a456-426614174001', 'open', 'Important Message', 'luis@pr.com', '2021-03-11T13:57:35.780Z');
