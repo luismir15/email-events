05209df5a6f9ee9f5c9297ee00a63b60f88db01a
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2022-08-30 12:15:57 -0400

    documenting Constants interface

--- src/main/java/com/example/emailevents/util/Constants.java
+++ src/main/java/com/example/emailevents/util/Constants.java
@@ -1,5 +1,11 @@
 package com.example.emailevents.util;
 
+/**
+ * The Constants interface will mainly be used to validate fields.
+ * event click and open constant will limit the action and the
+ * email pattern is to verify is the recipient has the correct
+ * syntax.
+ */
 public interface Constants {
 
     String EVENT_CLICK = "click";
