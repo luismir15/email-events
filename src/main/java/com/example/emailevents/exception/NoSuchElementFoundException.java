0beee52d85e87c8ebfd151216ae393b1801652d9
Luis Miranda <41742635+luismir15@users.noreply.github.com>
2022-08-30 12:13:16 -0400

    documenting custom exception class

--- src/main/java/com/example/emailevents/exception/NoSuchElementFoundException.java
+++ src/main/java/com/example/emailevents/exception/NoSuchElementFoundException.java
@@ -1,7 +1,16 @@
 package com.example.emailevents.exception;
 
+/**
+ * Custom Exception extending the RuntimeException class. This
+ * exception will mainly be used if the GET request could not
+ * perform properly or if an empty object was to be returned
+ */
 public class NoSuchElementFoundException extends RuntimeException {
 
+    /**
+     * Constructor to set message
+     * @param message error
+     */
     public NoSuchElementFoundException(String message){
         super(message);
     }
